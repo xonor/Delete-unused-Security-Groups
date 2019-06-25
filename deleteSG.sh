624d4f6d09fb8bdb720f7cf2e5f612274f2cafb1
xonor <xonoroneill@gmail.com>
2019-06-25 14:47:46 +0100

    Update deleteSG.sh
    14:47 June 25th 2019

--- deleteSG.sh
+++ deleteSG.sh
@@ -1,1 +1,7 @@
-for i in $(aws ec2 describe-security-groups --query 'SecurityGroups[*].GroupId'  --output text --region eu-west-1); do $(aws ec2 delete-security-group --group-id $i --region eu-west-1); done
+#!/bin/bash
+#Delete unused Security Groups
+
+read -p 'Region: ' userRegion
+for SG in $(aws ec2 describe-security-groups --query 'SecurityGroups[*].GroupId'  --output text --region $userRegion); do
+	aws ec2 delete-security-group --group-id $SG --region $userRegion
+done
